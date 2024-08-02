44aa011116881c13fefa25f661576357b10adba0
pnaik1 <pnaik@redhat.com>
2024-08-02 16:42:06 +0530

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,59 @@
-import * as vscode from 'vscode';
-import { exec } from 'child_process';
+import * as vscode from "vscode";
+import { exec } from "child_process";
+import path from "path";
+const odhDashboardDirectory = path.resolve(
+  __dirname,
+  "../../odh-dashboard/frontend"
+);
+// const currentDirectory = path.resolve(__dirname, "../..");
+const cypressRun = `cd ${odhDashboardDirectory} && npm i && npm run cypress:server:dev`;
+const cypressExecute = `cd ${odhDashboardDirectory} && npm i && npm run cypress:open:mock`;
 
+let myStatusBarItem: vscode.StatusBarItem;
+let isStarted = false;
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.startCypressServer', () => {
-        exec('npx cypress open', (error, stdout, stderr) => {
+  myStatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  myStatusBarItem.text = "Cypress Server";
+  myStatusBarItem.tooltip = "Start/Stop the Cypress Server";
+  myStatusBarItem.command = "extension.startCypressServer";
+  myStatusBarItem.show();
+
+  let disposable = vscode.commands.registerCommand(
+    "extension.startCypressServer",
+    () => {
+      isStarted = !isStarted;
+      if (isStarted) {
+        exec(
+          cypressRun,
+          { cwd: odhDashboardDirectory },
+          (error, stdout, stderr) => {
+            if (stdout) {
+              vscode.window.showInformationMessage(`Cypress Server started`);
+            }
             if (error) {
-                vscode.window.showErrorMessage(`Error: ${error.message}`);
-                return;
+              vscode.window.showErrorMessage(`Error: ${error.message}`);
+              return;
             }
             if (stderr) {
-                vscode.window.showErrorMessage(`Error: ${stderr}`);
-                return;
+              vscode.window.showErrorMessage(`Error: ${stderr}`);
+              return;
             }
-            vscode.window.showInformationMessage(`Cypress Server Started: ${stdout}`);
-        });
-    });
+            vscode.window.showInformationMessage(
+              `Cypress Server Started: ${stdout}`
+            );
+          }
+        );
+      } else {
+        vscode.window.showInformationMessage(`Cypress Server stopped `);
+      }
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(myStatusBarItem);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
